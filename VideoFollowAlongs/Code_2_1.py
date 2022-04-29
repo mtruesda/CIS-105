41384c9cba6797ded3c4f277a405160752c7fd8a
Myron Truesdale <99936347+mtruesda@users.noreply.github.com>
2022-04-29 18:41:15 -0400

    Delete Code_2_1.py

--- VideoFollowAlongs/Code_2_1.py
+++
@@ -1,9 +0,0 @@
-print('Hello World')
-print()
-print('Hi again')
-
-#
-#2.3 went over print statements, the different quotations (',",''') and their
-#uses. It then followed up with talking about making comments.
-#2/10/2020
-#
